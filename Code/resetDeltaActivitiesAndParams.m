84fc77716b8a17100c56b4f2bf3e89d98616e519
David Eriksson <50678197+David-Eriksson@users.noreply.github.com>
2019-11-29 08:16:57 +0100

    Update resetDeltaActivitiesAndParams.m

--- Code/resetDeltaActivitiesAndParams.m
+++ Code/resetDeltaActivitiesAndParams.m
@@ -6,6 +6,6 @@ for dai=1:length(g_deltaActivities)
     g_deltaActivities{dai}(:) = 0;
 end
 
-for dai=1:length(g_deltaParameters)
-    g_deltaParameters{dai}(:) = 0;
+for dpi=1:length(g_deltaParameters)
+    g_deltaParameters{dpi}(:) = 0;
 end
